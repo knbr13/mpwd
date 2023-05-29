f871a41bf65039bb10432ab94d9a883bfd9c44f8
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-29 22:46:32 +0300

    add error and loading states

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -62,7 +62,7 @@ const Register = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">iLine</span>
+        <span className="logo">Lama Chat</span>
         <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
           <input required type="text" placeholder="display name" />
@@ -73,7 +73,9 @@ const Register = () => {
             <img src={Add} alt="" />
             <span>Add an avatar</span>
           </label>
-          <button>Sign up</button>
+          <button disabled={loading}>Sign up</button>
+          {loading && "Uploading and compressing the image please wait..."}
+          {err && <span>Something went wrong</span>}
         </form>
         <p>
           You do have an account? <Link to="/register">Login</Link>
