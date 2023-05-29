8097d8faa14432037c8d752451443d0630ff47bf
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-29 23:32:47 +0300

    fetch messages and display messages

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,23 +1,27 @@
-const Messages = ({ message }) => {
+import { doc, onSnapshot } from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
+import { ChatContext } from "../context/ChatContext";
+import { db } from "../firebase";
+import Message from "./Message";
+
+const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [data.chatId]);
   return (
-    <div
-      className={`message ${message.senderId === currentUser.uid && "owner"}`}
-    >
-      <div className="messageInfo">
-        <img
-          src={
-            message.senderId === currentUser.uid
-              ? currentUser.photoURL
-              : data.user.photoURL
-          }
-          alt=""
-        />
-        <span>just now</span>
-      </div>
-      <div className="messageContent">
-        <p>{message.text}</p>
-        {message.img && <img src={message.img} alt="" />}
-      </div>
+    <div className="messages">
+      {messages.map((m) => (
+        <Message message={m} key={m.id} />
+      ))}
     </div>
   );
 };
