8b4c6d619cb6f1279677337a3d7ea7fecc4dc914
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-29 23:25:11 +0300

    add useEffect hook to load chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,4 +1,29 @@
+import { doc, onSnapshot } from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+
 const Chats = () => {
+  const [chats, setChats] = useState([]);
+
+  const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+  
   return (
     <div className="chats">
       <div className="userChat">
