fdab6417d02064c270c4569ab9fb1d513912aacd
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-29 22:58:32 +0300

    use AuthContext in App.jsx to for protecting routes purpose

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { useContext } from "react";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "./style.scss";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  let currentUser;
+  const { currentUser } = useContext(AuthContext);
   const ProtectedRoute = ({ children }) => {
     if (!currentUser) {
       return <Navigate to="/login" />;
