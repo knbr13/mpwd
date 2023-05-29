e3220a7f6866a533b1a1fb7e9c3c769a31cc68f2
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-29 23:40:49 +0300

    add send handler to Send button

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -74,14 +74,24 @@ const Input = () => {
   };
   return (
     <div className="input">
-      <input type="text" placeholder="Type something..." />
+      <input
+        type="text"
+        placeholder="Type something..."
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
       <div className="send">
         <img src={Attach} alt="" />
-        <input type="file" style={{ display: "none" }} id="file" />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="file"
+          onChange={(e) => setImg(e.target.files[0])}
+        />
         <label htmlFor="file">
           <img src={Img} alt="" />
         </label>
-        <button>Send</button>
+        <button onClick={handleSend}>Send</button>
       </div>
     </div>
   );
