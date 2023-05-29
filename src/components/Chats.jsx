ee569b471910b11d8c3eb7ac35e6513edbdd5e20
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-29 19:42:29 +0300

    create Chats component

---
+++ src/components/Chats.jsx
@@ -0,0 +1,15 @@
+const Chats = () => {
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img src={""} alt="" />
+        <div className="userChatInfo">
+          <span>chat name</span>
+          <p>last message</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chats;
