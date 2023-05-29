387d37bfa8031dc9af9b9d10f350675eae705914
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-29 19:24:40 +0300

    Create Login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">iLine</span>
+        <span className="title">Login</span>
+        <form>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign in</button>
+        </form>
+        <p>Don't have an account? <Link to="/register">Register</Link></p>
+      </div>
+    </div>
+  )
+}
+
+export default Login
