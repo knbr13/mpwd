334ec73d31deb26560cbd1f93a21ec1801f209e7
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-30 00:02:20 +0300

    remove required constraint to file input

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -53,6 +53,7 @@ const Register = () => {
         });
       });
     } catch (err) {
+      console.log(err.message)
       setErr(true);
       setLoading(false);
     }
@@ -67,7 +68,7 @@ const Register = () => {
           <input required type="text" placeholder="display name" />
           <input required type="email" placeholder="email" />
           <input required type="password" placeholder="password" />
-          <input required style={{ display: "none" }} type="file" id="file" />
+          <input style={{ display: "none" }} type="file" id="file" />
           <label htmlFor="file">
             <img src={Add} alt="" />
             <span>Add an avatar</span>
