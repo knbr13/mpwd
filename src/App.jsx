e28d4e46c0bf1495fdfbe07da548bf6dccd82350
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-29 22:56:09 +0300

    add routing to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "./style.scss";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+  let currentUser;
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
   return (
-    <div>
-      Hello World
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
