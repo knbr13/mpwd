13c8f5f6a9331964835d58252ec9e7ee7df0908b
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-29 22:25:29 +0300

    Create Chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,24 @@
+import Cam from "../img/cam.png";
+import Add from "../img/add.png";
+import More from "../img/more.png";
+import Messages from "./Messages";
+import Input from "./Input";
+
 const Chat = () => {
   return (
-    <div>Chat</div>
-  )
-}
+    <div className="chat">
+      <div className="chatInfo">
+        <span>username</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
