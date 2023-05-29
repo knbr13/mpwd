0c2189441fc4ff56224f8001944dc31e6e082515
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-29 23:08:40 +0300

    implement handleSearch handler

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,26 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../firebase";
+import { useState } from "react";
+
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
   return (
     <div className="search">
       <div className="searchForm">
