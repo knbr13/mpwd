ab3296ade0c9d374091931592580c4d4d214d092
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-29 23:28:06 +0300

    add dynamic data using Chat Context instead of hardcoded data

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,12 +3,15 @@ import Add from "../img/add.png";
 import More from "../img/more.png";
 import Messages from "./Messages";
 import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
+import { useContext } from "react";
 
 const Chat = () => {
+  const { data } = useContext(ChatContext);
   return (
     <div className="chat">
       <div className="chatInfo">
-        <span>username</span>
+        <span>{data.user?.displayName}</span>
         <div className="chatIcons">
           <img src={Cam} alt="" />
           <img src={Add} alt="" />
