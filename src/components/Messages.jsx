b311fee456eadf691bbcc37e6c86fd685ec09a35
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-29 23:28:45 +0300

    create messages component

---
+++ src/components/Messages.jsx
@@ -0,0 +1,25 @@
+const Messages = ({ message }) => {
+  return (
+    <div
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}
+    >
+      <div className="messageInfo">
+        <img
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
+      </div>
+    </div>
+  );
+};
+
+export default Messages;
