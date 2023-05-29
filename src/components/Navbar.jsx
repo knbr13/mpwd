921261fb2f3ced529d5885ae48a72ddbe46bf10c
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-29 23:05:08 +0300

    add dynamic data in the Navbar instead of hardcoded data

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,20 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <span className="logo">iLine</span>
       <div className="user">
-        <img src={""} alt="" />
-        <span>username</span>
-        <button>logout</button>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
+        <button onClick={() => signOut(auth)}>logout</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
