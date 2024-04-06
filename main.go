1a08edb73e0be9c27d8e68495ff9fc84babc561d
knbr13 <abdullahalaadine63@gmail.com>
2024-04-06 17:33:54 +0300

    reset

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"log"
-
-	"github.com/gofiber/fiber/v2"
-)
-
 func main() {
-	r := fiber.New()
 
-	r.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello world\n")
-	})
-	log.Fatal(r.Listen(":3000"))
 }
