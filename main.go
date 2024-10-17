7507cc2275d9b3ed8fdc48524ddb9afdd327dc92
knbr13 <abdullahalaadine63@gmail.com>
2024-10-18 02:37:42 +0300

    add Server struct

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
+import (
+	"net/url"
+	"sync"
+)
+
 type Config struct {
 	HealthCheckInterval string   `json:"healthCheckInterval"`
 	Servers             []string `json:"servers"`
 	ListenPort          string   `json:"listenPort"`
+}
+
+type Server struct {
+	URL               *url.URL   // URL of the backend server.
+	ActiveConnections int        // Count of active connections
+	Mutex             sync.Mutex // A mutex for safe concurrency
+	Healthy           bool
 }
