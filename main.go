953384f5e7fc62afda768826c511aed804bd177a
knbr13 <abdullahalaadine63@gmail.com>
2024-10-18 03:12:06 +0300

    add main func

--- main.go
+++ main.go
@@ -2,11 +2,62 @@ package main
 
 import (
 	"encoding/json"
+	"log"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
 	"sync"
+	"time"
 )
+
+func main() {
+	config, err := loadConfig("config.json")
+	if err != nil {
+		log.Fatalf("Error loading configuration: %s", err.Error())
+	}
+
+	healthCheckInterval, err := time.ParseDuration(config.HealthCheckInterval)
+	if err != nil {
+		log.Fatalf("Invalid health check interval: %s", err.Error())
+	}
+
+	var servers []*Server
+	for _, serverUrl := range config.Servers {
+		u, _ := url.Parse(serverUrl)
+		servers = append(servers, &Server{URL: u})
+	}
+
+	for _, server := range servers {
+		go func(s *Server) {
+			for range time.Tick(healthCheckInterval) {
+				res, err := http.Get(s.URL.String())
+				if err != nil || res.StatusCode >= 500 {
+					s.Healthy = false
+				} else {
+					s.Healthy = true
+				}
+			}
+		}(server)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		server := nextServerLeastActive(servers)
+		server.Mutex.Lock()
+		server.ActiveConnections++
+		server.Mutex.Unlock()
+		server.Proxy().ServeHTTP(w, r)
+		server.Mutex.Lock()
+		server.ActiveConnections--
+		server.Mutex.Unlock()
+	})
+
+	log.Println("Starting server on port", config.ListenPort)
+	err = http.ListenAndServe(config.ListenPort, nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %s\n", err)
+	}
+}
 
 type Config struct {
 	HealthCheckInterval string   `json:"healthCheckInterval"`
